d3eee7bac5570a15146289b7351cc1f1977daf3b
Quentin-Allen Velayo Fernandez <qfernandez@csumb.edu>
2020-02-05 00:21:32 -0800

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Quentin-Allen Velayo Fernandez on 2/5/20.
+//  Copyright © 2020 Maryann Cortez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
